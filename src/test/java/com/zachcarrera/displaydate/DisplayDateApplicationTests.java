cb755b1d15dd4f1a26cdfd33586eb5670a445c5c
Zach Carrera <zachcarrera@gmail.com>
2023-04-10 14:54:31 -0700

    add test directory

---
+++ src/test/java/com/zachcarrera/displaydate/DisplayDateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zachcarrera.displaydate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisplayDateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
