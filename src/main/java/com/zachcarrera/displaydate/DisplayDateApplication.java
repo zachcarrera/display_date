99f44f2f8656a4e89bb2c53b31cdc05514583de3
Zach Carrera <zachcarrera@gmail.com>
2023-04-10 14:50:37 -0700

    add app and ServletInitializer class

---
+++ src/main/java/com/zachcarrera/displaydate/DisplayDateApplication.java
@@ -0,0 +1,13 @@
+package com.zachcarrera.displaydate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisplayDateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisplayDateApplication.class, args);
+	}
+
+}
